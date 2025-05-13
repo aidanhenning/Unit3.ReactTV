1dd3ad2846b0e615892b1daa43d953299c11ff3c
aidanhenning <henningaidan@gmail.com>
2025-05-12 17:22:13 -0700

    initial commit

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,26 @@
+import { useState } from "react";
+import EpisodeDetails from "../episodes/EpisodeDetails";
+import EpisodeList from "../episodes/EpisodeList";
 import "./shows.css";
 
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+
+export default function ShowDetails({ show }) {
+  const [selectedEpisode, setSelectedEpisode] = useState(null);
+
+  if (!show) {
+    return <p>Select a show to view episodes</p>;
+  }
+
+  return (
+    <div className="show-details">
+      <EpisodeList
+        name={show.name}
+        episodes={show.episodes}
+        selectedEpisode={selectedEpisode}
+        setSelectedEpisode={setSelectedEpisode}
+      />
+      <EpisodeDetails episode={selectedEpisode} />
+    </div>
+  );
 }
